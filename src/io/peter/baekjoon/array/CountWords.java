db35175d869747b8d86cb8bd7c8b1d0c8ebc8597
Peter Choi <cbj0618@gmail.com>
2018-01-21 23:11:27 +0900

    1. 단어 개수 카운팅 문제 예외상황 반영 예외상황 1 : trim()없이 (공백)a 입력 시, token[0]에 ""가, token[1]에 a가 저장되어 2개로 카운팅 됨. 예외상황 2 : 단어 사이에 공백이 늘어 나면, split(" ")을 사용한 경우 공백 하나하나를 단어로 카운팅 함. 그래서 StringTokenizer로 바꿨음.
    2. 속도 개선
    Scanner보다는 BufferedReader를 쓰는게 속도가 빠름!!

--- src/io/peter/baekjoon/array/CountWords.java
+++ src/io/peter/baekjoon/array/CountWords.java
@@ -1,12 +1,26 @@
 package io.peter.baekjoon.array;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CountWords {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String sentence = sc.nextLine();
-		System.out.println(sentence.split(" ").length);
-		sc.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String sentence = bufferedReader.readLine();
+		System.out.println(new StringTokenizer(sentence).countTokens());
+		
+//		String sentence = bufferedReader.readLine().trim();
+//		if(sentence.equals(""))
+//			System.out.println(0);
+//		else
+//			System.our.println(sentence.split(" ").length);
 	}
 }
+
+/* 공백을 하나 입력한 상태에서 trim을 하면 ""이 남음. 따라서 이 경우 0을 리턴하도록 해야 함. 
+ * (StringTokenizer를 이용하는 경우, 위 경우는 자연스럽게 해결해줌)
+ */
+
+ 
